1ac6585af304ebc5f75af584512da9ba22a7a34f
kellaahn <kellaahn@g.hongik.ac.kr>
2026-05-09 17:03:24 +0900

    upload

--- sketch.js
+++ sketch.js
@@ -181,8 +181,27 @@ function draw() {
 
      //여기다가 첫화면 애니매이션 넣으면 되고
     
-     tint(255, 255);
-     
+
+ 
+ 
+  imageMode(CENTER);
+  tint(255, 20);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 2800, 4300 ); 
+  tint(255, 30);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 2600, 4100 ); 
+  tint(255, 40);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 2400, 3900 );  
+  tint(255, 60);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 2200, 3700 );  
+  tint(255, 70);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 1900, 3400 );  
+  tint(255, 80);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 1600, 2800 );  
+  tint(255, 150);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 1200, 2000 );  
+ 
+  tint(255, 255);
+  image(matryoImg,  windowWidth/2  , windowHeight/2  , 900, 1500 );  
     
     
  
